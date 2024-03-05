ca01c5899ec5622aea443c27d4b4f58a606edf02
Mae B. Morella <4561733+mmorella-dev@users.noreply.github.com>
2024-03-05 05:00:18 -0500

    fisher: replace cargo config with plugins

---
+++ .config/fish/conf.d/cargo_abbrevs.fish
@@ -0,0 +1,9 @@
+# Abbrevs
+abbr cb "cargo build"
+abbr ch "cargo check"
+abbr ct "cargo test"
+abbr cr "cargo run"
+abbr crr "cargo run --release"
+abbr rup "rustup update"
+abbr cwt "cargo watch -x test --quiet"
+abbr cta "cargo test && cargo test -- --ignored"
