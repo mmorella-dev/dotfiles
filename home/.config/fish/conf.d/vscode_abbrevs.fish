35d8c5c04e0ea05121c90002de4ef83c53388de3
Mae B. Morella <mmorella-dev@pm.me>
2024-09-01 11:36:46 -0400

    Vscode abbrevs: prefer VS Code over Codium

--- home/.config/fish/conf.d/vscode_abbrevs.fish
+++ home/.config/fish/conf.d/vscode_abbrevs.fish
@@ -1,10 +1,10 @@
 # vscode abbreviations
 # based on robbyrussel's vscode plugin for oh-my-zsh
 if not test -n "$VSCODE"
-    if command -q codium
-        set VSCODE codium
-    else if command -q code
+    if command -q code
         set VSCODE code
+    else if command -q code
+        set VSCODE codium
     else if command -q code-insiders
         set VSCODE code-insiders
     end
